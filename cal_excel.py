cd4143b85b44ef0d98826cb98e1940ee599919b6
Liberbell <liberbell@gmail.com>
2025-05-28 08:58:10 +0900

    cal_excel4

--- cal_excel.py
+++ cal_excel.py
@@ -16,4 +16,7 @@ wb_test3 = openpyxl.load_workbook("excel/bill_test3.xlsx")
 ws_test3 = wb_test3.worksheets[0]
 
 ws.cell(2, 1).value = ws_test1.cell(3, 14).value
+ws.cell(2, 2).value = ws_test1.cell(3, 1).value
+ws.cell(2, 3).value = ws_test1.cell(15, 4).value
+ws.cell(2, 4).value = ws_test1.cell(4, 14).value
 wb.save("excel/bill_list.xlsx")
