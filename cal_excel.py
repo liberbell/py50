37acaf2503bd44f6f1081c19d1e45637caa94e2f
Liberbell <liberbell@gmail.com>
2025-05-29 08:18:55 +0900

    cal_excel2

--- cal_excel.py
+++ cal_excel.py
@@ -7,11 +7,25 @@ ws = wb["Sheet1"]
 wb_test1 = openpyxl.load_workbook("excel/bill_test1.xlsx", data_only=True)
 ws_test1 = wb_test1["Sheet1"]
 
-wb_test2 = openpyxl.load_workbook("excel/bill_test2.xlsx", data_only=True)
-ws_test2 = wb_test2.worksheets[0]
+files = sorted(glob("excel/bill_test*.xlsx"))
 
-wb_test3 = openpyxl.load_workbook("excel/bill_test3.xlsx")
-ws_test3 = wb_test3.worksheets[0]
+for file in files:
+
+    wb_test = openpyxl.load_workbook(file, data_only=True)
+    ws_test = wb_test.worksheets[0]
+
+    ws.cell(2, 1).value = ws_test.cell(3, 14).value
+    ws.cell(2, 2).value = ws_test.cell(3, 1).value
+    ws.cell(2, 3).number_format = r"¥#,##0;¥-#,##0"
+    ws.cell(2, 3).value = ws_test.cell(15, 4).value
+    ws.cell(2, 4).number_format = r"yyyy/m/d"
+    ws.cell(2, 4).value = ws_test.cell(4, 14).value
+
+# wb_test2 = openpyxl.load_workbook("excel/bill_test2.xlsx", data_only=True)
+# ws_test2 = wb_test2.worksheets[0]
+
+# wb_test3 = openpyxl.load_workbook("excel/bill_test3.xlsx")
+# ws_test3 = wb_test3.worksheets[0]
 
 # ws.cell(2, 1).value = ws_test1.cell(3, 14).value
 # ws.cell(2, 2).value = ws_test1.cell(3, 1).value
@@ -27,8 +41,7 @@ ws_test3 = wb_test3.worksheets[0]
 # ws.cell(3, 4).number_format = r"yyyy/m/d"
 # ws.cell(3, 4).value = ws_test2.cell(4, 14).value
 
-files = glob("excel/bill_test*.xlsx")
-print(files)
+
 
 
 wb.save("excel/bill_list.xlsx")
