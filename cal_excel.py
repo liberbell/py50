7028f6c5015760c71a2f55fe12120793f3a9e67c
Liberbell <liberbell@gmail.com>
2025-05-28 09:04:20 +0900

    cal_excel5

--- cal_excel.py
+++ cal_excel.py
@@ -3,7 +3,7 @@ import openpyxl
 wb = openpyxl.load_workbook("excel/bill_list.xlsx")
 ws = wb["Sheet1"]
 
-wb_test1 = openpyxl.load_workbook("excel/bill_test1.xlsx")
+wb_test1 = openpyxl.load_workbook("excel/bill_test1.xlsx", data_only=True)
 ws_test1 = wb_test1["Sheet1"]
 print(ws_test1.cell(row=4, column=7).value)
 print(ws_test1.cell(3, 14).value)
