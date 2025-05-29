7e3762387c94b3ea9ddd299d82106834ad9054cb
Liberbell <liberbell@gmail.com>
2025-05-30 08:35:36 +0900

    combine6

--- lesson2/combine.py
+++ lesson2/combine.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 df = pd.read_excel("excel_files/sell_list.xlsx", sheet_name="Sheet1")
 
-print(df["売上金額"])
-print(df["売上金額"].mean())
-print(df["売上金額"].sum())
-print(df["売上金額"].min())
-print(df["売上金額"].max())
+# print(df["売上金額"])
+# print(df["売上金額"].mean())
+# print(df["売上金額"].sum())
+# print(df["売上金額"].min())
+# print(df["売上金額"].max())
 
-print(df[["担当者", "売上金額"]])
+# print(df[["担当者", "売上金額"]])
 
+print(df)
 print(df.iloc[2, 3])
+df.iloc[2, 3] = "bob"
+print(df)
