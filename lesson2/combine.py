2c2ccebd997cc25ba564a10cb691d7c53e0b5964
Liberbell <liberbell@gmail.com>
2025-05-30 08:27:08 +0900

    combine3

--- lesson2/combine.py
+++ lesson2/combine.py
@@ -3,3 +3,7 @@ import pandas as pd
 df = pd.read_excel("excel_files/sell_list.xlsx", sheet_name="Sheet1")
 
 print(df["売上金額"])
+print(df["売上金額"].mean())
+print(df["売上金額"].sum())
+print(df["売上金額"].min())
+print(df["売上金額"].max())
