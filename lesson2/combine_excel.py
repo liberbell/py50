51d401fc81adbba0fac6f4e649a2795f47a2e6ff
Liberbell <liberbell@gmail.com>
2025-06-01 11:00:07 +0900

    combine_excel3

--- lesson2/combine_excel.py
+++ lesson2/combine_excel.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 files = glob("excel_files/revenue_*.xlsx")
 
-df_tanaka = pd.read_excel(files[0], sheet_name="Sheet1")
-df_sato = pd.read_excel(files[1], sheet_name="Sheet1")
+df_concat = pd.DataFrame()
 
-df_concat = pd.concat([df_tanaka, df_sato], ignore_index=True)
+for file in files:
+    df_temp = pd.read_excel(file, sheet_name="Sheet1")
+    df_concat = pd.concat([df_concat, df_temp])
