32da12bc626551b22b054ff60ae3844af2cdc01b
Liberbell <liberbell@gmail.com>
2025-06-02 08:40:00 +0900

    export_sheet6

--- lesson2/export_sheet.py
+++ lesson2/export_sheet.py
@@ -9,6 +9,10 @@ df_name_unique = df_renew["担当者"].unique()
 for name in df_name_unique:
     df_temp = df_renew[df_renew["担当者"] == name]
     revenue_sum = df_temp["売上金額"].sum()
-    
+    sheet_name = name + "_" + str(revenue_sum) + "yen"
+
     print(df_temp)
     print(name + "の売上合計: ", revenue_sum)
+
+    with pd.ExcelWriter("excel_files/revenue_new.xlsx", mode="a") as f:
+        df_temp.to_excel(f, sheet_name=sheet_name, index=False)
