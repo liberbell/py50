d2f94d20e3a183efa55180359cda3e15e46d8aa6
Liberbell <liberbell@gmail.com>
2025-06-02 08:30:13 +0900

    export_sheet3

--- lesson2/export_sheet.py
+++ lesson2/export_sheet.py
@@ -3,5 +3,11 @@ import pandas as pd
 df = pd.read_excel("excel_files/sell_list_new.xlsx", sheet_name="まとめ", header=2)
 print(df)
 
-df_renew =df.iloc[0:len(df)-2, 1:9]
-print(df_renew)
+df_renew =df.iloc[0:len(df)-2, 1:9].dropna()
+df_name_unique = df_renew["担当者"].unique()
+
+for name in df_name_unique:
+    df_temp = df_renew[df_renew["担当者"] == name]
+    
+    print(df_temp)
+    print(df_temp["売上金額"].sum())
