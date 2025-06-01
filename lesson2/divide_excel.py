d77625bb8a02157c4cef1f1d2a0aec3b6a47944e
Liberbell <liberbell@gmail.com>
2025-06-01 10:44:07 +0900

    divide_excel3

--- lesson2/divide_excel.py
+++ lesson2/divide_excel.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_excel("excel_files/sell_list.xlsx", sheet_name="Sheet1")
-print(df)
+# print(df)
 
 print(df[df["担当者"] == "田中"])
 
@@ -14,6 +14,9 @@ print(df[(df["担当者"] == "田中") | (df["売上金額"] >= 300000)].reset_i
 print(df[(df["担当者"] == "田中") | (df["売上金額"] >= 300000)].iloc[40, 3])
 
 name_list = ["田中", "佐藤", "中村", "鈴木"]
-for name in name_list:
-    df_name = df[df["担当者"] == name].reset_index(drop=True)
-    df_name.to_excel(f"excel_files/revenue_{name}.xlsx", index=False)
+df_name_unique = df["担当者"].unique()
+print(name_list)
+print(df_name_unique)
+# for name in name_list:
+#     df_name = df[df["担当者"] == name].reset_index(drop=True)
+#     df_name.to_excel(f"excel_files/revenue_{name}.xlsx", index=False)
