36251b7aa8dd62c480d607956a00fde33b3be895
Liberbell <liberbell@gmail.com>
2025-06-01 11:07:58 +0900

    combine_excel6

--- lesson2/combine_excel.py
+++ lesson2/combine_excel.py
@@ -2,6 +2,7 @@ from glob import glob
 import pandas as pd
 
 files = glob("excel_files/revenue_*.xlsx")
+print(files)
 
 df_concat = pd.DataFrame()
 
@@ -9,4 +10,4 @@ for file in files:
     df_temp = pd.read_excel(file, sheet_name="Sheet1")
     df_concat = pd.concat([df_concat, df_temp])
 
-df_concat.to_excel("excel_files/revenue_all.xlsx", index=False)
+df_concat.sort_values("No.").to_excel("excel_files/revenue_all.xlsx", index=False)
