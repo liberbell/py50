2f7232fa5eaa95c643622d65fc4f80ceac8d2525
Liberbell <liberbell@gmail.com>
2025-06-01 10:30:07 +0900

    divide_excel1

--- lesson2/divide_excel.py
+++ lesson2/divide_excel.py
@@ -15,7 +15,7 @@ print(df[(df["担当者"] == "田中") | (df["売上金額"] >= 300000)].iloc[40
 
 df_tanaka = df[df["担当者"] == "田中"].reset_index(drop=True)
 print(df_tanaka)
-df_tanaka.to_excel("excel_files/revenue_tanaka.xlsx",)
+df_tanaka.to_excel("excel_files/revenue_tanaka.xlsx", index=False)
 
 df_nakamura = df[df["担当者"] == "中村"].reset_index(drop=True)
 print(df_nakamura)
