5abea393189e869ec1615c817b06611a2dea8f12
Liberbell <liberbell@gmail.com>
2025-06-01 11:01:43 +0900

    combine_excel4

--- lesson2/combine_excel.py
+++ lesson2/combine_excel.py
@@ -8,3 +8,5 @@ df_concat = pd.DataFrame()
 for file in files:
     df_temp = pd.read_excel(file, sheet_name="Sheet1")
     df_concat = pd.concat([df_concat, df_temp])
+
+df_concat.to_clipboard("excel_files/revenue_all.xlsx", index=False)
