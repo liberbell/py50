e837068e6a875e3247f4f7f81c42b456fc3991f0
Liberbell <liberbell@gmail.com>
2025-06-02 08:20:29 +0900

    export_sheet2

--- lesson2/export_sheet.py
+++ lesson2/export_sheet.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
-df = pd.read_excel("excel_files/sell_list_new.xlsx", sheet_name="まとめ")
+df = pd.read_excel("excel_files/sell_list_new.xlsx", sheet_name="まとめ", header=2)
+print(df)
+
+df_renew =df.iloc[0:len(df)-2, 1:9]
+print(df_renew)
