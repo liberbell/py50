771f2b32b23868f99ed2283d118e7b4625bd6586
Liberbell <liberbell@gmail.com>
2025-06-01 10:55:08 +0900

    combine_excel2

--- lesson2/combine_excel.py
+++ lesson2/combine_excel.py
@@ -1,3 +1,9 @@
 from glob import glob
+import pandas as pd
 
 files = glob("excel_files/revenue_*.xlsx")
+
+df_tanaka = pd.read_excel(files[0], sheet_name="Sheet1")
+df_sato = pd.read_excel(files[1], sheet_name="Sheet1")
+
+df_concat = pd.concat([df_tanaka, df_sato], ignore_index=True)
