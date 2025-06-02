47698e7579d1798c9e58a7d3386e513da85aae62
Liberbell <liberbell@gmail.com>
2025-06-03 08:11:37 +0900

    excel_vlookup1

--- lesson3/excel_vlookup.py
+++ lesson3/excel_vlookup.py
@@ -1,4 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv("excel_files/history_mar.csv", encoding="shift-jis")
-print(df)
+df_history1 = pd.read_csv("excel_files/history_mar.csv", encoding="shift-jis", engine="python")
+print(df_history1)
+
+df_division = pd.read_csv("excel_files/division_list.csv")
