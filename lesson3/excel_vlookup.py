b1b6a66326d7b9e8b626f0262d198bf58412aab0
Liberbell <liberbell@gmail.com>
2025-06-03 08:23:30 +0900

    excel_vlookup6

--- lesson3/excel_vlookup.py
+++ lesson3/excel_vlookup.py
@@ -7,4 +7,4 @@ df_division = pd.read_excel("excel_files/division_list.xlsx", engine="openpyxl",
 print(df_division)
 
 df_merge = pd.merge(df_history1, df_division, on="社員番号")
-print(df_merge.iloc[:, [0, 1, 2, 3, 4, 5, 6, 7, 9]])
+print(df_merge.iloc[:, [0, 1, 2, 9, 3, 4, 5, 6, 7]])
