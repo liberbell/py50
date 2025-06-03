9603cf63c385580cb2294d672f2b17ffb79f7cfa
Liberbell <liberbell@gmail.com>
2025-06-04 08:38:53 +0900

    differncial_detect1

---
+++ lesson3/differncial_detect.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df_history_mar = pd.read_csv("excel_files/history_mar.csv", encoding="shift-jis", engine="python")
+df_history_may = pd.read_csv("excel_files/history_may.csv", encoding="shift-jis", engine="python")
