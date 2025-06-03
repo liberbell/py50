6c0462f788e59ec403a79ace25b998860c9d7e19
Liberbell <liberbell@gmail.com>
2025-06-04 08:20:54 +0900

    excel_vlookup2

--- lesson3/excel_vlookup.py
+++ lesson3/excel_vlookup.py
@@ -10,10 +10,11 @@ df_merge = pd.merge(df_history1, df_division, on="社員番号")
 df_merge = df_merge.iloc[:, [0, 1, 2, 9, 3, 4, 5, 6, 7]].sort_values("ID")
 
 df_merge2 = pd.merge(df_history1, df_division, left_on="担当者", right_on="名前")
-df_merge2 = df_merge2.iloc[:, [0, 1, 2, 9, 3, 4, 5, 6, 7]].sort_values("ID")
+print(df_merge2)
+df_merge2 = df_merge2.iloc[:, [0, 1, 2, 11, 3, 4, 5, 6, 7]].sort_values("ID")
 
 # df_merge.to_excel("excel_files/merge_history_division.xlsx", index=False)
 # df_merge.to_csv("excel_files/merge_history_division.csv", index=False, encoding="shift-jis")
 
-df_merge2.to_excel("excel_files/merge_history_division2.xlsx", index=False)
-df_merge2.to_csv("excel_files/merge_history_division2.csv", index=False, encoding="shift-jis")
+# df_merge2.to_excel("excel_files/merge_history_division2.xlsx", index=False)
+# df_merge2.to_csv("excel_files/merge_history_division2.csv", index=False, encoding="shift-jis")
