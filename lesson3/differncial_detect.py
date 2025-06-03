ef9020577bb83a229df20aeb3fa31bf9b7e46beb
Liberbell <liberbell@gmail.com>
2025-06-04 08:59:32 +0900

    differncial_detect3

--- lesson3/differncial_detect.py
+++ lesson3/differncial_detect.py
@@ -2,5 +2,7 @@ import pandas as pd
 
 df_history_mar = pd.read_csv("excel_files/history_mar.csv", encoding="shift-jis", engine="python")
 df_history_may = pd.read_csv("excel_files/history_may.csv", encoding="shift-jis", engine="python")
-print(df_history_mar)
-print(df_history_may)
+# print(df_history_mar)
+# print(df_history_may)
+
+df_merge = pd.merge(df_history_may, df_history_may, on="ID", how="outer", indicator=True)
