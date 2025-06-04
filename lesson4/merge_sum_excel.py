c09255b6b4d6bc9dea611c3322fa39584fab62c1
Liberbell <liberbell@gmail.com>
2025-06-05 08:44:41 +0900

    merge_sum_excel3

--- lesson4/merge_sum_excel.py
+++ lesson4/merge_sum_excel.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
-df_rev_list = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl")
+df_rev_mar = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="4月")
+df_rev_may = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="5月")
+df_rev_jun = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="6月")
