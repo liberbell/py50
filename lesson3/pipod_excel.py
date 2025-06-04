228808bef96638fd284c85000f622b82d8249a27
Liberbell <liberbell@gmail.com>
2025-06-05 08:29:04 +0900

    pipod_excel2

--- lesson3/pipod_excel.py
+++ lesson3/pipod_excel.py
@@ -1,3 +1,4 @@
 import pandas as pd
 
 df_history_mar = pd.read_csv("excel_files/history_mar.csv", encoding="shift-jis", engine="python")
+df_pivot = df_history_mar.pivot_table(index="担当者", columns="取引先", values="対応工数(h)", aggfunc="sum")
