a7f3bfcfb038a4e2cc7f3f087c5df4c7ca4e5b1b
Liberbell <liberbell@gmail.com>
2025-06-05 09:04:00 +0900

    merge_sum_excel7

--- lesson4/merge_sum_excel.py
+++ lesson4/merge_sum_excel.py
@@ -5,6 +5,10 @@ print(df_rev_mar)
 print(len(df_rev_mar))
 line = 0
 for line in range(len(df_rev_mar)):
-    print(df_rev_mar.iloc[line, 5])
+    price = df_rev_mar.iloc[line, 5]
+    num = df_rev_mar.iloc[line, 6]
+    sum_price = price * num
+    print(sum_price)
+    line += 1
 df_rev_may = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="5月")
 df_rev_jun = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="6月")
