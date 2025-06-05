bdf55b708b09b9faf6a3791277a9c168136093e4
Liberbell <liberbell@gmail.com>
2025-06-06 08:25:37 +0900

    merge_sum_excel7

--- lesson4/merge_sum_excel.py
+++ lesson4/merge_sum_excel.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
 month_list = ["4月", "5月", "6月"]
-sheet_name = pd.ExcelFile("excel_files/revenue_permon.xlsx")
-print(sheet_name.sheet_names)
-
+sheet_name = pd.ExcelFile("excel_files/revenue_permon.xlsx").sheet_names
 
 df_concat = pd.DataFrame()
 
-for month in month_list:
+for month in sheet_name:
     df_month = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name=month)
     df_concat = pd.concat([df_concat, df_month])
 
