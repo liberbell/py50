bd09faa54f0f3739bcae01f0b6dedeee268c5b79
Liberbell <liberbell@gmail.com>
2025-06-06 08:16:50 +0900

    merge_sum_excel1

--- lesson4/merge_sum_excel.py
+++ lesson4/merge_sum_excel.py
@@ -1,14 +1,22 @@
 import pandas as pd
 
-df_rev_mar = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="4月")
-print(df_rev_mar)
-print(len(df_rev_mar))
-line = 0
-for line in range(len(df_rev_mar)):
-    price = df_rev_mar.iloc[line, 5]
-    num = df_rev_mar.iloc[line, 6]
-    sum_price = price * num
-    print(sum_price)
-    line += 1
-df_rev_may = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="5月")
-df_rev_jun = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="6月")
+month_list = ["4月", "5月", "6月"]
+df_concat = pd.DataFrame()
+
+for month in month_list:
+    df_concat = pd.read_excel("excel_files/revenue_permonth.xlsx", engine="openpyxl", sheet_name=month)
+
+print(df_concat)
+
+# df_rev_mar = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="4月")
+# print(df_rev_mar)
+# print(len(df_rev_mar))
+# line = 0
+# for line in range(len(df_rev_mar)):
+#     price = df_rev_mar.iloc[line, 5]
+#     num = df_rev_mar.iloc[line, 6]
+#     sum_price = price * num
+#     print(sum_price)
+#     line += 1
+# df_rev_may = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="5月")
+# df_rev_jun = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="6月")
