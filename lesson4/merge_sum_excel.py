e14309c074daad072affd59d87b9fd3260448ff6
Liberbell <liberbell@gmail.com>
2025-06-05 09:01:36 +0900

    merge_sum_excel6

--- lesson4/merge_sum_excel.py
+++ lesson4/merge_sum_excel.py
@@ -3,7 +3,8 @@ import pandas as pd
 df_rev_mar = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="4月")
 print(df_rev_mar)
 print(len(df_rev_mar))
-for line in len(df_rev_mar):
+line = 0
+for line in range(len(df_rev_mar)):
     print(df_rev_mar.iloc[line, 5])
 df_rev_may = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="5月")
 df_rev_jun = pd.read_excel("excel_files/revenue_permon.xlsx", engine="openpyxl", sheet_name="6月")
