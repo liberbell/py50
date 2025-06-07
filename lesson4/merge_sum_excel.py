008c345239d45ba575f84a3cd11fe75f664d45c6
Liberbell <liberbell@gmail.com>
2025-06-07 10:49:17 +0900

    merge_sum_excel9

--- lesson4/merge_sum_excel.py
+++ lesson4/merge_sum_excel.py
@@ -11,17 +11,16 @@ for month in sheet_name:
 
 df_concat["売上金額"] = df_concat["単価"] * df_concat["個数"]
 
-df_employee = pd.read_excel("excel_files/employee_list.xlsx")
-# df_employee["社員番号"] = df_employee["社員番号"].str.replace("A-", "")
+df_employee = pd.read_excel("excel_files/employee_list.xlsx", sheet_name="リスト")
+
 df_employee["社員番号"] = df_employee["社員番号"].str.replace("[A-Z]-", "", regex=True)
 df_employee = df_employee.astype({"社員番号":int})
 
 df_merge_employee = pd.merge(df_concat, df_employee, on="社員番号")
 df_merge_employee = df_merge_employee.iloc[:, [0, 1, 2, 3, 8, 4, 5, 6, 7]]
-print(df_merge_employee)
 
 df_name_list = df_merge_employee["名前"].unique()
-print(df_name_list)
+
 for name in df_name_list:
     df_divide_employee = df_merge_employee[df_merge_employee["名前"] == name]
     df_pivot = df_divide_employee.pivot_table(index="取引先", columns="商品名", values="売上金額", aggfunc="sum", fill_value=0, margins=True, margins_name="総計")
